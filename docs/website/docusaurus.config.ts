6faa263d668664355d78ddbe823ccb87b8634e73
Matt Hamann <mhamann@rownd.io>
2025-08-13 15:41:49 -0400

    fix(docs): set deploy branch

--- docs/website/docusaurus.config.ts
+++ docs/website/docusaurus.config.ts
@@ -25,6 +25,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'llama-farm', // Usually your GitHub org/user name.
   projectName: 'llama-farm.github.io', // Usually your repo name.
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
