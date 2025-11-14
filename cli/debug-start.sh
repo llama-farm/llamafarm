4f7ad8e1046b624313ebd2f3092008f8fcd3b1dc
Bobby Radford <bobby@rownd.io>
2025-11-14 16:43:44 -0500

    chore(cli): remove debug-start.sh

--- cli/debug-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Build with debug symbols and disable optimizations
-echo "Building with debug symbols..."
-go build -gcflags="all=-N -l" -ldflags="-compressdwarf=false" -o lf main.go
-
-# Start with Delve, waiting for debugger to attach
-echo "Starting lf with Delve on port 2345..."
-echo "Attach your debugger, then the process will start"
-dlv exec ./lf --headless --listen=:2345 --api-version=2 --accept-multiclient --check-go-version=false -- "$@"
