9739f7c5c22894dc88daa082cbf259567f80fcd9
Bobby Radford <bobby@rownd.io>
2025-11-19 11:52:20 -0500

    fix(designer): missing import

--- designer/src/components/Rag/Databases.tsx
+++ designer/src/components/Rag/Databases.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { useQueryClient } from '@tanstack/react-query'
 import FontIcon from '../../common/FontIcon'
