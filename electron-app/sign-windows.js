88f8a36510aeea24ef75a1ec15b5e5f8486edd37
Matt Hamann <mhamann@rownd.io>
2025-11-24 21:47:02 -0500

    chore: import pwsh module before signing

--- electron-app/sign-windows.js
+++ electron-app/sign-windows.js
@@ -45,9 +45,10 @@ exports.default = async function (configuration) {
   const normalizedPath = path.resolve(filePath).replace(/\//g, '\\');
 
   // Build the PowerShell command with proper escaping
-  // NOTE: We don't quote the path in -Files because Invoke-TrustedSigning
+  // NOTE: We import the module first, then call Invoke-TrustedSigning
+  // We don't quote the path in -Files because Invoke-TrustedSigning
   // expects an unquoted path or an array of paths
-  const psCommand = `Invoke-TrustedSigning -Endpoint '${endpoint}' -CertificateProfileName '${certificateProfile}' -CodeSigningAccountName '${accountName}' -TimestampRfc3161 'http://timestamp.acs.microsoft.com' -TimestampDigest 'SHA256' -FileDigest 'SHA256' -Files '${normalizedPath}'`;
+  const psCommand = `Import-Module Microsoft.Windows.TrustedSigning -ErrorAction Stop; Invoke-TrustedSigning -Endpoint '${endpoint}' -CertificateProfileName '${certificateProfile}' -CodeSigningAccountName '${accountName}' -TimestampRfc3161 'http://timestamp.acs.microsoft.com' -TimestampDigest 'SHA256' -FileDigest 'SHA256' -Files '${normalizedPath}'`;
 
   const command = `pwsh.exe -NoProfile -NonInteractive -Command "${psCommand}"`;
 
